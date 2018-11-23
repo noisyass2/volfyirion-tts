7d7e845f8732eb0a8dd173e02762dca185835bf1
Bernard Rivera <bernard.rivera.m@gmail.com>
2018-11-23 08:43:42 +0800

    added nodejs stuff

---
+++ nodejssandbox.js
@@ -0,0 +1,38 @@
+var fs = require('fs')
+var LINQ = require('node-linq').LINQ;
+
+
+function check()
+{
+    var metaJson = fs.readFileSync("csvjson.json");
+    var metadata = JSON.parse(metaJson);
+
+    var deckJson = fs.readFileSync("VolfyDeckWithName.json");
+    
+    var deck = JSON.parse(deckJson.toString());
+    var cards = deck.ObjectStates[0];
+
+    console.log(cards);
+    cards.ContainedObjects.forEach(card => {
+        var meta = new LINQ(metadata)
+        .Where(function(c) { return c.Name + '\r\n' === card.Nickname; })
+        .First();
+
+        if(meta)
+        {
+            card.LuaScriptState = JSON.stringify(meta);
+            
+        }
+        else{
+            console.log("NOT FOUND : " + card.Nickname);
+            console.log(card);
+        }
+    });
+
+    fs.writeFileSync("DeckWithMeta.json",JSON.stringify(deck));
+    console.log("DONE");
+    //console.log(arr);
+}
+
+
+check();
